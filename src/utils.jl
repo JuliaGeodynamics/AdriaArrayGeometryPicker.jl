7fcc3bb63ea87c922df4079d76225777be1223a9
Marcel Thielmann <thielmann.marcel@gmail.com>
2025-10-16 10:42:15 +0200

    Comment out logo insertion functions
    Comment out the insert_logo! and insert_logo_GMG! functions in utils.jl.

--- src/utils.jl
+++ src/utils.jl
@@ -1,29 +1,29 @@
 # this is utils.jl
 # it collects all the utility functions used in the GUI that do not belong to a specific category
 
-function insert_logo!(panel)
-    logo_img = load("./assets/AdA_GeometryPicker_logo_tr.png")
-    ax_logo = Axis(panel[1,1],aspect = DataAspect())
-    image!(ax_logo,rotr90(logo_img))
-    #ax_logo.yreversed = true
-    hidedecorations!(ax_logo)
-    ax_logo.leftspinevisible=false
-    ax_logo.rightspinevisible=false
-    ax_logo.topspinevisible=false
-    ax_logo.bottomspinevisible=false
-    return nothing
-end
+# function insert_logo!(panel)
+#     logo_img = load("./assets/AdA_GeometryPicker_logo_tr.png")
+#     ax_logo = Axis(panel[1,1],aspect = DataAspect())
+#     image!(ax_logo,rotr90(logo_img))
+#     #ax_logo.yreversed = true
+#     hidedecorations!(ax_logo)
+#     ax_logo.leftspinevisible=false
+#     ax_logo.rightspinevisible=false
+#     ax_logo.topspinevisible=false
+#     ax_logo.bottomspinevisible=false
+#     return nothing
+# end
 
-function insert_logo_GMG!(panel)
-    img = load("./assets/GMG_Logo_new.png")
-    ax_logo = Axis(panel[1,1],aspect = DataAspect())
-    image!(ax_logo,img')
-    ax_logo.yreversed = true
-    hidedecorations!(ax_logo)
-    ax_logo.leftspinevisible=false
-    ax_logo.rightspinevisible=false
-    ax_logo.topspinevisible=false
-    ax_logo.bottomspinevisible=false
-    return nothing
-end
+#  function insert_logo_GMG!(panel)
+#     img = load("./assets/GMG_Logo_new.png")
+#     ax_logo = Axis(panel[1,1],aspect = DataAspect())
+#     image!(ax_logo,img')
+#     ax_logo.yreversed = true
+#     hidedecorations!(ax_logo)
+#     ax_logo.leftspinevisible=false
+#     ax_logo.rightspinevisible=false
+#     ax_logo.topspinevisible=false
+#     ax_logo.bottomspinevisible=false
+#     return nothing
+# end
 
