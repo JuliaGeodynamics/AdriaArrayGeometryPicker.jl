e2ee77de9934cc5a3b6689d3d353342ae8453f44
Marcel Thielmann <thielmann.marcel@gmail.com>
2026-09-24 15:51:41 +0200

    only use the basename of the profile file to display

--- src/AdA_VizPickTool_V1.jl
+++ src/AdA_VizPickTool_V1.jl
@@ -223,7 +223,7 @@ function start_AdA_Picker(;data=nothing)
                 text!(topo_ax1,minimum(x_topo),maximum(y_topo);text = string(round.(data.start_lonlat,digits=2)),align = (:left, :center),offset = (20, 0))
                 text!(topo_ax1,maximum(x_topo),maximum(y_topo);text = string(round.(data.end_lonlat,digits=2)), align = (:right, :center),offset = (-20, 0))
                 # display the profile name in the title
-                topo_ax1.title = fn
+                topo_ax1.title = basename(fn)
 
                 # plot layout
                 rowgap!(panel_plot,0) # no vertical space between topo and profile plot
