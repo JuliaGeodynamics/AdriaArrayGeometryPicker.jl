9ec7aaf398452bca180a50281793686f78868e31
Marcel Thielmann <thielmann.marcel@gmail.com>
2026-09-24 15:38:33 +0200

    added axis title to topo axis

--- src/AdA_VizPickTool_V1.jl
+++ src/AdA_VizPickTool_V1.jl
@@ -223,8 +223,9 @@ function start_AdA_Picker(;data=nothing)
                 text!(topo_ax1,minimum(x_topo),maximum(y_topo);text = string(round.(data.start_lonlat,digits=2)),align = (:left, :center),offset = (20, 0))
                 text!(topo_ax1,maximum(x_topo),maximum(y_topo);text = string(round.(data.end_lonlat,digits=2)), align = (:right, :center),offset = (-20, 0))
 
+                topo_ax1.title = "profilename"
                 # display the profile name in the middle of the topography plot
-                text!(topo_ax1,0.5*(maximum(x_topo)-minimum(x_topo)),maximum(y_topo);text = "profilename", align = (:center, :center),offset = (0, 20))
+                #text!(topo_ax1,minimum(x_topo),maximum(y_topo);text = "profilename", align = (:center, :center),offset = (0, 20))
 
                 # plot layout
                 rowgap!(panel_plot,0) # no vertical space between topo and profile plot
